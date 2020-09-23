d491648296c17a3c5d2ad1f679cd47c053eb0c35
mike andrews <mra@xoba.com>
2020-09-23 13:19:24 -0400

    approximating inflation with big.Int's

--- infl/infl.go
+++ infl/infl.go
@@ -32,40 +32,53 @@ func Run(c cnfg.Config) error {
 
 type Balance struct {
 	Height int
-	Amount *big.Rat
+	Amount *big.Int
+}
+
+func cp(i *big.Int) *big.Int {
+	var x big.Int
+	return x.Set(i)
+}
+
+func approx(r *big.Rat) *big.Int {
+	var z big.Int
+	z.Div(r.Num(), r.Denom())
+	return &z
 }
 
 // inflation with big numbers
 func RunBig(c cnfg.Config) error {
 	var height int
 	var list []Balance
-	add := func(i *big.Rat) {
+	add := func(i *big.Int) {
 		list = append(list, Balance{
 			Height: height,
-			Amount: i,
+			Amount: cp(i),
 		})
 	}
 	const t0 = 100
 
-	//	unit := big.NewInt(1000000)
+	unit := big.NewInt(100000000)
 	f0 := big.NewRat(t0, t0+1)
 	fnum := f0.Num()
 	fdenom := f0.Denom()
 
-	value := func(b Balance) *big.Rat {
+	value := func(b Balance) *big.Int {
 		dt := big.NewInt(int64(height - b.Height))
 		var fnum2, fdenom2 big.Int
 		fnum2.Exp(fnum, dt, nil)
 		fdenom2.Exp(fdenom, dt, nil)
 		var f big.Rat
 		f.SetFrac(&fnum2, &fdenom2)
+		var b0 big.Rat
+		b0.SetFrac(b.Amount, big.NewInt(1))
 		var b2 big.Rat
-		b2.Mul(b.Amount, &f)
-		return &b2
+		b2.Mul(&b0, &f)
+		return approx(&b2)
 	}
 
-	totalValue := func() *big.Rat {
-		var total big.Rat
+	totalValue := func() *big.Int {
+		var total big.Int
 		for _, b := range list {
 			total.Add(&total, value(b))
 		}
@@ -78,10 +91,13 @@ func RunBig(c cnfg.Config) error {
 	}
 
 	for {
-		add(big.NewRat(1, 1))
+		add(unit)
 		height++
-		v, _ := totalValue().Float64()
+		var units big.Rat
+		units.SetFrac(totalValue(), unit)
+		v, _ := units.Float64()
 		fmt.Printf("height = %d, total value = %.20f\n", height, v)
+		time.Sleep(time.Millisecond)
 	}
 
 	return nil
