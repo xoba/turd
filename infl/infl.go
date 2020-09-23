30df970884381966d7be6f643d1d08f6a83f5fbb
mike andrews <mra@xoba.com>
2020-09-23 14:11:54 -0400

    transferring a bunch of balances over to a new one

--- infl/infl.go
+++ infl/infl.go
@@ -48,96 +48,100 @@ func approx(r *big.Rat) *big.Int {
 	return &z
 }
 
+type Config struct {
+	Height          int
+	InflationFactor *big.Rat
+	Balances        map[string]Balance
+}
+
+func (c Config) value(b Balance) *big.Int {
+	fnum := c.InflationFactor.Num()
+	fdenom := c.InflationFactor.Denom()
+	dt := big.NewInt(int64(c.Height - b.Height))
+	var fnum2, fdenom2 big.Int
+	fnum2.Exp(fnum, dt, nil)
+	fdenom2.Exp(fdenom, dt, nil)
+	var f big.Rat
+	f.SetFrac(&fnum2, &fdenom2)
+	var b0 big.Rat
+	b0.SetFrac(b.Amount, big.NewInt(1))
+	var b2 big.Rat
+	b2.Mul(&b0, &f)
+	return approx(&b2)
+}
+func (c Config) totalValue() *big.Int {
+	var total big.Int
+	for _, b := range c.Balances {
+		total.Add(&total, c.value(b))
+	}
+	return &total
+}
+
+func (c Config) add(id string, i *big.Int) {
+	c.Balances[id] = Balance{
+		ID:     id,
+		Amount: i,
+		Height: c.Height,
+	}
+}
+
+func (c Config) xfer(to, from string) error {
+	fromBalance, ok := c.Balances[from]
+	if !ok {
+		return fmt.Errorf("no such source balance: %s", from)
+	}
+	toBalance, ok := c.Balances[to]
+	if !ok {
+		toBalance = Balance{
+			ID:     to,
+			Height: c.Height,
+			Amount: big.NewInt(0),
+		}
+	}
+	toValue := c.value(toBalance)
+	fromValue := c.value(fromBalance)
+	delete(c.Balances, from)
+	delete(c.Balances, to)
+	var total big.Int
+	total.Add(toValue, fromValue)
+	c.add(to, &total)
+	return nil
+}
+
 // inflation with big numbers
-func RunBig(c cnfg.Config) error {
-	const t0 = 100
-	unit := big.NewInt(100000000)
-	f0 := big.NewRat(t0, t0+1)
-
-	var height int
-
-	balances := make(map[string]Balance)
-
-	newBalance := func(id string, i *big.Int) Balance {
-		return Balance{
-			ID:     id,
-			Height: height,
-			Amount: cp(i),
-		}
+func RunBig(cnfg.Config) error {
+	const (
+		t0   = 100
+		unit = 100000000
+	)
+	c := Config{
+		Height:          0,
+		InflationFactor: big.NewRat(t0, t0+1),
+		Balances:        make(map[string]Balance),
 	}
-
-	add := func(id string, i *big.Int) {
-		balances[id] = newBalance(id, i)
-	}
-
-	value := func(b Balance) *big.Int {
-		fnum := f0.Num()
-		fdenom := f0.Denom()
-		dt := big.NewInt(int64(height - b.Height))
-		var fnum2, fdenom2 big.Int
-		fnum2.Exp(fnum, dt, nil)
-		fdenom2.Exp(fdenom, dt, nil)
-		var f big.Rat
-		f.SetFrac(&fnum2, &fdenom2)
-		var b0 big.Rat
-		b0.SetFrac(b.Amount, big.NewInt(1))
-		var b2 big.Rat
-		b2.Mul(&b0, &f)
-		return approx(&b2)
-	}
-
-	totalValue := func(m map[string]Balance) *big.Int {
-		var total big.Int
-		for _, b := range m {
-			total.Add(&total, value(b))
-		}
-		return &total
-	}
-
-	xfer := func(to, from string) error {
-		fromBalance, ok := balances[from]
-		if !ok {
-			return fmt.Errorf("no such source balance: %s", from)
-		}
-		toBalance, ok := balances[to]
-		if !ok {
-			toBalance = newBalance(to, big.NewInt(0))
-		}
-		toValue := value(toBalance)
-		fromValue := value(fromBalance)
-
-		delete(balances, from)
-		delete(balances, to)
-
-		var total big.Int
-		total.Add(toValue, fromValue)
-
-		add(to, &total)
-		return nil
-	}
-
 	for {
-		add(uuid.New().String(), unit)
-		height++
+		c.add(uuid.New().String(), big.NewInt(unit))
+		c.Height++
 		var units big.Rat
-		units.SetFrac(totalValue(balances), unit)
+		units.SetFrac(c.totalValue(), big.NewInt(unit))
 		v, _ := units.Float64()
-		fmt.Printf("height = %d, total value = %.20f; len=%d\n", height, v, len(balances))
-		if len(balances) > 100 {
-			for i := 0; i < 5; i++ {
-				var from, to string
-			LOOP:
-				for k := range balances {
-					switch {
-					case from == "":
-						from = k
-					case to == "":
-						to = k
-					default:
-						break LOOP
-					}
+		fmt.Printf("height = %5d, total value = %.20f; len=%d\n", c.Height, v, len(c.Balances))
+		if len(c.Balances) > 100 {
+			const n = 5
+			// choose n balances to transfer from:
+			var from []string
+		LOOP:
+			for k := range c.Balances {
+				switch {
+				case len(from) < n:
+					from = append(from, k)
+				default:
+					break LOOP
 				}
-				if err := xfer(to, from); err != nil {
+			}
+			id := uuid.New().String()
+			for _, f := range from {
+				if err := c.xfer(id, f); err != nil {
 					return err
 				}
 			}
