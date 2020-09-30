819be0941a757b5ed6717ae70703835a1010fe62
mike andrews <mra@xoba.com>
2020-09-30 08:19:39 -0400

    more rigorously testing the tree against go's native map

--- trie/trie.go
+++ trie/trie.go
@@ -1,9 +1,12 @@
 package trie
 
 import (
+	"bytes"
 	"encoding/asn1"
+	"encoding/json"
 	"fmt"
 	"math/rand"
+	"sort"
 	"strings"
 	"time"
 
@@ -12,7 +15,7 @@ import (
 )
 
 type Trie struct {
-	kv   *keyValue
+	kv   *KeyValue
 	hash []byte
 	next [256]*Trie
 }
@@ -21,20 +24,97 @@ func New() *Trie {
 	return &Trie{}
 }
 
+type Database interface {
+	Set([]byte, []byte)
+	Get([]byte) ([]byte, bool)
+	Delete([]byte)
+	Do(func(kv *KeyValue))
+}
+
+type Iterable interface {
+	Do(func(kv *KeyValue))
+}
+
+type mapdb map[string][]byte
+
+func (m mapdb) Set(key []byte, value []byte) {
+	m[string(key)] = value
+}
+
+func (m mapdb) Get(key []byte) ([]byte, bool) {
+	v, ok := m[string(key)]
+	return v, ok
+}
+
+func (m mapdb) Delete(key []byte) {
+	delete(m, string(key))
+}
+
+func (m mapdb) Do(f func(*KeyValue)) {
+	var list []*KeyValue
+	for k, v := range m {
+		list = append(list, &KeyValue{
+			Key:   []byte(k),
+			Value: v,
+		})
+	}
+	sort.Slice(list, func(i, j int) bool {
+		return bytes.Compare(list[i].Key, list[j].Key) == -1
+	})
+	for _, kv := range list {
+		f(kv)
+	}
+}
+
+func String(i Iterable) string {
+	var list []*KeyValue
+	i.Do(func(kv *KeyValue) {
+		list = append(list, kv)
+	})
+	buf, _ := json.Marshal(list)
+	return string(buf)
+}
+
 func Run(cnfg.Config) error {
+	const (
+		n = 3
+		m = 1000000
+	)
+	newBuf := func() []byte {
+		buf := make([]byte, 1+rand.Intn(n))
+		rand.Read(buf)
+		return buf
+	}
+	var db1, db2 Database
+	db1 = make(mapdb)
+	db2 = New()
+	for i := 0; i < m; i++ {
+		k, v := newBuf(), newBuf()
+		db1.Set(k, v)
+		db2.Set(k, v)
+	}
+	if String(db1) != String(db2) {
+		return fmt.Errorf("mismatch")
+	}
+	return nil
+}
+
+func Run2(cnfg.Config) error {
 	rand.Seed(0)
 	const (
 		idlen = 16
-		n     = 700
+		n     = 7000
 	)
-	var ids []keyValue
-	for i := 0; i < n; i++ {
-		buf := make([]byte, idlen)
-		rand.Read(buf)
-		ids = append(ids, keyValue{
-			Key:   buf,
-			Value: buf[:1+rand.Intn(idlen-1)],
-		})
+	var ids []KeyValue
+	{
+		for i := 0; i < n; i++ {
+			buf := make([]byte, idlen)
+			rand.Read(buf)
+			ids = append(ids, KeyValue{
+				Key:   buf,
+				Value: buf, // buf[:1+rand.Intn(idlen-1)],
+			})
+		}
 	}
 	if true {
 		rand.Seed(time.Now().UTC().UnixNano())
@@ -50,9 +130,7 @@ func Run(cnfg.Config) error {
 		id, prefix := kv.Key, kv.Value
 		all[string(id)] = string(prefix)
 		m[string(prefix)] = true
-		if err := t.Set(prefix, id); err != nil {
-			return err
-		}
+		t.Set(prefix, id)
 	}
 	for id, prefix := range all {
 		_, ok := t.Get([]byte(prefix))
@@ -66,18 +144,20 @@ func Run(cnfg.Config) error {
 			}
 		}
 	}
-	h, err := t.ComputeHash()
-	if err != nil {
-		return err
-	}
-	fmt.Printf("trie hash: 0x%x\n", h)
+	fmt.Printf("trie hash: 0x%x\n", t.ComputeHash())
 	fmt.Printf("%v per iteration\n", time.Since(start)/n)
 	return nil
 }
 
-func (t *Trie) ComputeHash() ([]byte, error) {
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func (t *Trie) ComputeHash() []byte {
 	if len(t.hash) > 0 {
-		return t.hash, nil
+		return t.hash
 	}
 	var list [][]byte
 	add := func(x []byte) {
@@ -88,29 +168,23 @@ func (t *Trie) ComputeHash() ([]byte, error) {
 		add(t.kv.Value)
 	}
 	for i, x := range t.next {
-		add([]byte{byte(i)})
 		if x == nil {
 			continue
 		}
-		h, err := x.ComputeHash()
-		if err != nil {
-			return nil, err
-		}
-		add(h)
+		add([]byte{byte(i)})
+		add(x.ComputeHash())
 	}
 	buf, err := asn1.Marshal(list)
-	if err != nil {
-		return nil, err
-	}
+	check(err)
 	t.hash = thash.Hash(buf)
-	return t.hash, nil
+	return t.hash
 }
 
-type keyValue struct {
+type KeyValue struct {
 	Key, Value []byte
 }
 
-func (kv keyValue) ComputeHash() ([]byte, error) {
+func (kv KeyValue) ComputeHash() ([]byte, error) {
 	buf, err := asn1.Marshal(kv)
 	if err != nil {
 		return nil, err
@@ -120,13 +194,13 @@ func (kv keyValue) ComputeHash() ([]byte, error) {
 
 func (t *Trie) String() string {
 	var list []string
-	t.Do(func(kv *keyValue) {
+	t.Do(func(kv *KeyValue) {
 		list = append(list, fmt.Sprintf("%q:%q", string(kv.Key), string(kv.Value)))
 	})
 	return strings.Join(list, ", ")
 }
 
-func (t *Trie) Do(f func(kv *keyValue)) {
+func (t *Trie) Do(f func(kv *KeyValue)) {
 	for _, x := range t.next {
 		if x == nil {
 			continue
@@ -157,7 +231,7 @@ func (t *Trie) Get(key []byte) ([]byte, bool) {
 	return current.kv.Value, true
 }
 
-func (t *Trie) Set(key, value []byte) error {
+func (t *Trie) Set(key, value []byte) {
 	t.hash = nil
 	current := t
 	for _, b := range key {
@@ -170,6 +244,9 @@ func (t *Trie) Set(key, value []byte) error {
 		}
 		current.hash = nil
 	}
-	current.kv = &keyValue{Key: key, Value: value}
-	return nil
+	current.kv = &KeyValue{Key: key, Value: value}
+}
+
+func (t *Trie) Delete(key []byte) {
+	check(fmt.Errorf("Delete unimplemented"))
 }
